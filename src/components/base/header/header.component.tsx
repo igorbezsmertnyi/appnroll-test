ddcd495efc50d5511952915726c303e161c7f28c
igorbezsmertnyi <mr.igor96@outlook.com>
2020-02-21 19:49:45 +0100

    Feature/implements header styles

--- src/components/base/header/header.component.tsx
+++ src/components/base/header/header.component.tsx
@@ -1,28 +1,32 @@
 import React from "react"
 import styled from "styled-components"
-import { backgroundColor } from "../../../theming/theme-getters"
+import Logo from "./logo/logo.component"
+import Info from "./info/info.component"
 import AppFunctionComponent from "../../../types/app-function-component.interface"
+import IOrganization from "../../../types/organization-interface"
 
 const HeaderRoot = styled.header`
-  background: ${backgroundColor("primary")};
   margin-bottom: 1.45rem;
+  padding: 71px 0 25px;
 `
 
 const ContentWrapper = styled.div`
   margin: 0 auto;
-  max-width: 960px;
+  display: flex;
+  max-width: 1155px;
   padding: 1.45rem 1.0875rem;
 `
 
 interface Props {
-  siteTitle: string
+  organization: IOrganization
 }
 
-const Header: AppFunctionComponent<Props> = ({ siteTitle }) => {
+const Header: AppFunctionComponent<Props> = ({ organization }) => {
   return (
     <HeaderRoot>
       <ContentWrapper>
-        <h1>{siteTitle}</h1>
+        <Logo src={organization.avatarUrl} />
+        <Info info={organization} />
       </ContentWrapper>
     </HeaderRoot>
   )
---
+++ src/components/base/header/logo/logo.component.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import styled from "styled-components"
+import AppFunctionComponent from "../../../../types/app-function-component.interface"
+import { backgroundColor } from "../../../../theming/theme-getters"
+
+const LogoWrapper = styled.div`
+  min-width: 102px;
+  width: 102px;
+  height: 102px;
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  background-color: ${backgroundColor("haiti")};
+`
+
+const LogoImage = styled.img`
+  width: calc(100% - 5px);
+  height: calc(100% - 5px);
+  object-fit: cover;
+`
+
+interface Props {
+  src: string
+}
+
+const Logo: AppFunctionComponent<Props> = ({ src }) => (
+  <LogoWrapper>
+    <LogoImage src={src} />
+  </LogoWrapper>
+)
+
+export default Logo
