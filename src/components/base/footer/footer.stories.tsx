edce3e0c920a7e74b86938d69e2dc9ef76e79500
igorbezsmertnyi <mr.igor96@outlook.com>
2020-02-21 20:26:44 +0100

    Feature/remove unused code

--- src/components/base/footer/footer.stories.tsx
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from "@storybook/react"
-import * as React from "react"
-import Footer from "./footer.component"
-
-storiesOf("Base|Footer", module).add("headers", () => (
-  <Footer currentYear="2077" />
-))
